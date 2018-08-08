06b984502c81ba98620549607484c8fe271457c5
Oseias <Oseiasanascimento@hotmail.com>
2018-08-08 01:35:25 -0300

    Adicionado o módulo PropTypes

--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import './index.css'
+import {Search} from './Components/BookSearch'
+import {Book} from './Components/Book'
+import PropTypes from 'prop-types'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<Search />, document.getElementById('root'))
