5aef06419ac86e4a494928a950d9916b7ef57b38
Oseias <Oseiasanascimento@hotmail.com>
2018-08-08 12:03:50 -0300

    Estrutura criada

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
 import React from 'react'
 import * as BooksAPI from './BooksAPI'
 import './App.css'
-import { Route } from "react-router-dom";
+import { Route } from "react-router-dom"
+import { Shelf } from './Components/BookShelf.js'
 
 class BooksApp extends React.Component {
-    state = {
-        books: {
-            currentlyReading: [],
-            wantToRead: [],
-            read: []
-        }
-    }
-
-    componentDidMount() {
-        this.getBooks();
-}
-
-    getBooks = () => {
-        //Aqui Pegarei os arquivos
-        console.log("Worked")
-    }
 
     render(){
-        return (<h1> Hello Darknes My Old Friend </h1>)
+        return (
+            <div className="list-books">
+              <div className="list-books-title">
+              <h1>MyReads</h1>
+              </div>
+
+                <Shelf bookShelfTitle="Currently Reading" />
+
+                <Shelf bookShelfTitle="Want To Read" />
+
+                <Shelf bookShelfTitle="Read" />
+
+                <div className="open-search">
+                    <a onClick={()=> this.setState({ showSearchPage: true })}>Add a book</a>
+                </div>
+
+             </div>
+
+        )
     }
 }
 export default BooksApp
