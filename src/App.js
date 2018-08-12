67dcf752652b4f87f8d48fb92c79487b3a13d3d9
Oseias <Oseiasanascimento@hotmail.com>
2018-08-12 19:56:48 -0300

    Armazenando cada livro em suas prateleiras

--- src/App.js
+++ src/App.js
@@ -6,18 +6,79 @@ import { Shelf } from './Components/BookShelf.js'
 
 class BooksApp extends React.Component {
 
+    state = {
+        books: [],
+        read: [],
+        wantToRead: [],
+        currentlyReading: []
+    }
+
+componentDidMount(){
+
+    BooksAPI.getAll().then((data) => {
+
+        this.setState({
+            books: data
+        })
+
+        console.log(data);
+
+    }).then(() => {
+
+        const bookList = this.state.books
+        const reads = []
+        const toRead = []
+        const reading = []
+
+        // Passar os lidos para o array
+            bookList.filter(a => {
+                return a.shelf == 'read';
+
+            }).map((book) => {
+                reads.push(book)
+                console.log(book);
+            })
+            this.setState({read: reads});
+
+            // Passar os want to read para o array
+            bookList.filter(
+                c => {
+                    return c.shelf === 'wantToRead';
+                }
+            ).map((book) => {
+                toRead.push(book)
+            })
+            this.setState({wantToRead: toRead});
+
+            // Passar os lendo agora  para o array
+            bookList.filter(
+                c => {
+                    return c.shelf === 'currentlyReading';
+                }
+            ).map((book) => {
+                reading.push(book)
+            })
+            this.setState({currentlyReading: reading});
+        })
+    }
+
+
+
+
+
     render(){
+
         return (
             <div className="list-books">
               <div className="list-books-title">
               <h1>MyReads</h1>
               </div>
 
-                <Shelf bookShelfTitle="Currently Reading" />
+                <Shelf shelfTitle="Currently Reading" shelf={this.state.currentlyReading} key="1"/>
 
-                <Shelf bookShelfTitle="Want To Read" />
+                <Shelf shelfTitle="Want To Read" shelf={this.state.wantToRead} key="2"/>
 
-                <Shelf bookShelfTitle="Read" />
+                <Shelf shelfTitle="Read" shelf={this.state.read} key="3" />
 
                 <div className="open-search">
                     <a onClick={()=> this.setState({ showSearchPage: true })}>Add a book</a>
