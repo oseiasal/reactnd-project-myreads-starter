80f3d848f0a2aba472bb4b5553f52237705e9226
Oseias <Oseiasanascimento@hotmail.com>
2018-08-16 00:08:51 -0300

    Eliminação dos módulos não utilizados

--- src/index.js
+++ src/index.js
@@ -2,11 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import './index.css'
-import {Search} from './Components/BookSearch'
-import {Book} from './Components/Book'
-import {Shelf} from './Components/BookShelf'
 import { BrowserRouter } from "react-router-dom";
-import PropTypes from 'prop-types'
 
 ReactDOM.render(
     <BrowserRouter><App /></BrowserRouter>, document.getElementById('root'))
