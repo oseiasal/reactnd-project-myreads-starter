2733ae6143d1811fa5c7d87cfd86fc1102908eb5
oseias <oseiasal@gmail.com>
2020-03-27 19:45:18 -0300

    updated and fixed

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import './index.css'
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.render(
-    <BrowserRouter><App /></BrowserRouter>, document.getElementById('root'))
+    <HashRouter><App /></HashRouter>, document.getElementById('root'))
